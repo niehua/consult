9bfa6af7f1396ffa98e5bf265ae2996dc52e848c
杨聂华 <niehua.yang@qunar.com>
2019-01-15 20:18:15 +0800

    t

--- src/main/java/niehua/consult/ConsultApplication.java
+++ src/main/java/niehua/consult/ConsultApplication.java
@@ -5,10 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConsultApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ConsultApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConsultApplication.class, args);
+    }
 
 }
 
